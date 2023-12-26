4f051ed1d3c8a85ab6b8f33c9b8864328d5c8e53
imrane sarsri <imransarsi.ii@gmail.com>
2023-12-26 13:13:01 +0100

    add lyoute

--- resources/views/Tasks/index.blade.php
+++ resources/views/Tasks/index.blade.php
@@ -1,3 +1,62 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+@extends('../Layouts.Layout')
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Liste des tâche</h1>
+                </div>
+                <div class="col-sm-6">
+                    <div class="float-sm-right">
+                        <a href="{{-- route('create') --}}" class="btn btn-sm btn-primary">Ajouter tâche</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            {{-- start alert --}}
+            @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Success </strong> {{ session('success') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+            {{-- end alert --}}
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header col-md-12">
+                            <div class=" p-0">
+                                <div class="input-group input-group-sm float-sm-right col-md-3 p-0">
+                                    <input type="text" id="table_search" name="table_search"
+                                        class="form-control float-right" placeholder="Search">
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-default">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="search_ajax">
+                            <div class="card-body table-responsive p-0">
+
+                                @include('../Layouts.Table')
+                            </div>
+                            <div class="d-flex justify-content-end align-items-center p-2">
+                                <div class="pagination  m-0 float-right">
+                                    {{-- {{ $Tasks->links() }} --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
