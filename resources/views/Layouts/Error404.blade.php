6a10384aa20856d347b5eacca46b4ebc594a6a2a
imrane sarsri <imransarsi.ii@gmail.com>
2023-12-26 13:09:00 +0100

    add maqquetage

--- resources/views/Layouts/Error404.blade.php
+++ resources/views/Layouts/Error404.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+<div class="wrapper">
+    <div class="text-center">
+        <h2>cette page n'est pas définie</h2>
+        <a href="{{ route('index') }}">Retour à la page d'accueil</a>
+    </div>
 </div>
--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+{{-- footer --}}
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+    </div>
+    <strong>Copyright © 2023-2024 <a href="https://adminlte.io">Solicoders</a>.</strong> Tous droits réservés.
+</footer>
